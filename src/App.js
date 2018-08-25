564ff24d460cea3a1d6910c6ecc40dea6c9f5f91
Daniela Cosmina Weigel <daniela.c.weigel@gmail.com>
2018-08-25 21:19:17 +0300

    Initialize the map

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state={
+    map: ''
+  }
+
+  initMap = () => {
+
+  let myLatLng = {lat: 41.9043799, lng: 12.4647467}
+  let view = document.getElementById('map');
+
+  const map = new window.google.maps.Map(view, {
+    center: myLatLng,
+    zoom: 13,
+  });
+}
 
   render() {
     return (
       <div className="App">
+        <div id="map">
 
+        </div>
       </div>
     );
   }
